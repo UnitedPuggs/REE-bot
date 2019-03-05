e3faf6d272e38ea5188c7ec6f8734b186e7c310b
UnitedPuggs <48199805+UnitedPuggs@users.noreply.github.com>
2019-03-04 21:59:56 -0800

    Update Ref.java

--- Ref.java
+++ Ref.java
@@ -1,6 +1,6 @@
 package com.REEBot;
 
 public class Ref {
-	public static final String token = "NTQ5MDgzODEzMzk4MDUyODc3.D1Ps5g.JajXsB39iQqzP-ngw6XGSNlEIm4";
+	public static final String token = "NTQ5MDgzODEzMzk4MDUyODc3.D1-XIg.rk64iD6YpV6yKCDhKXPwNE3JyjI";
 	public static final String prefix = "!";
 }
